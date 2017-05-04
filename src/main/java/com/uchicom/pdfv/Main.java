884d83d2bfb956ceeb208ea89ca6da2a58b9d5a0
Shigeki Uchiyama <shigeki@wk2>
2017-05-04 21:38:29 +0900

    初期コミット

---
+++ src/main/java/com/uchicom/pdfv/Main.java
@@ -0,0 +1,18 @@
+// (c) 2017 uchicom
+package com.uchicom.pdfv;
+
+/**
+ * @author uchicom: Shigeki Uchiyama
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+
+	}
+
+}
---
+++ src/main/java/com/uchicom/pdfv/PdfViewer.java
@@ -0,0 +1,22 @@
+// (c) 2017 uchicom
+package com.uchicom.pdfv;
+
+import javax.swing.JFrame;
+
+/**
+ * @author uchicom: Shigeki Uchiyama
+ *
+ */
+public class PdfViewer extends JFrame {
+
+	public PdfViewer() {
+		initComponents();
+	}
+
+	/**
+	 *
+	 */
+	private void initComponents() {
+		pack();
+	}
+}
